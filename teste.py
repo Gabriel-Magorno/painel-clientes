5790af7df14ec1f0a7f5c47345dd10b9936dfecf
Gabriel-Magorno <141511717+Gabriel-Magorno@users.noreply.github.com>
2025-05-13 11:50:46 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,9 @@
-import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
 import openpyxl
 
 df = pd.read_excel('ReceitaMa.xlsx')
 
-st.title('Dashboard de Clientes')
-
-st.subheader('Resumo dos dados')
-st.write(df.describe())
+df.show
 
